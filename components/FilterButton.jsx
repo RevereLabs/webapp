7849fc8d7f17bb8a64e270d0c3a45e4dd3561426
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-09-12 17:34:55 +0530

    minor

--- components/FilterButton.jsx
+++ components/FilterButton.jsx
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import Image from 'next/image'
 import Filter from './others/Filter'
 
-function FilterButton({filters,setFilters,isFilter=true}) {
+function FilterButton({filters,setFilters}) {
     console.log(filters)
     const FilterTypes = [{title:'Hardcoded',color:'#85A9E8'},{title:'Development',color:'#85B9E8'},{title:'Design',color:'#FFC8C8'}]
     const[isOpen,setIsOpen] = useState(false)
@@ -17,7 +17,7 @@ function FilterButton({filters,setFilters,isFilter=true}) {
         <div className="mx-10 relative z-[0] m-10 overflow-visible">
             <div onClick={()=>setIsOpen(preVal=>!preVal)}
                 className="z-[10] relative  w-[10rem]  h-[2rem] flex items-center justify-around border-2 rounded-md font-mada cursor-pointer drop-shadow-[5px_5px_0px_rgba(0,0,0,1)] active:drop-shadow-[1px_1px_0px_rgba(0,0,0,1)] bg-[#ffffff]">
-                <h2 className="font-mada text-textSecondary font-[600] text-[1.2rem]">{isFilter?'Filters':'Categories'}</h2>
+                <h2 className="font-mada text-textSecondary font-[600] text-[1.2rem]">Filters</h2>
                 {isOpen?<Image src={'/vectors/upArrow.png'}
                     height={10}
                     width={20}/>:
@@ -40,7 +40,7 @@ function FilterButton({filters,setFilters,isFilter=true}) {
                         <Filter title={item.title}
                             color={item.color}
                             filters={filters}
-                            key={i}/>
+                        key={i}/>
                     </div>
                 ))}
             </div>}
